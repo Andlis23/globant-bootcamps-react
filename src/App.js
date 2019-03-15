76b2a4f120f0ab9a3f71afcdc912b071211c4886
Andrha <you@example.com>
2019-03-15 17:25:58 -0300

    Semana 1

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Header  from './Header';
+import CarList from './CarList';
+import Footer  from './Footer';
+
+const App = () => (
+  <div className="App">
+    <Header />
+    <CarList />
+    <Footer />
+  </div>
+)
+
+export default App;
---
+++ src/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Footer.css'
+
+const Footer = () => (
+    <div className = "Footer">
+        <div className="Contact">
+            <a href='#'>About</a><br/><br/> 
+            <a href='#'>Contact us</a>
+        </div>
+        <div className="Copy">
+            <p>© 2019</p>
+        </div>
+    </div>
+)
+
+export default Footer;
