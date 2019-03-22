c5684c632e5a26c3b1ec4ef4b29c6a9abe6dc8d8
Andrha <you@example.com>
2019-03-21 23:02:51 -0300

    Router and Form

--- carproject/src/Pagina.js
+++ carproject/src/Pagina.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Pagina = ({brand,model,year,motor}) => (
     <div>
-        <h1>{brand} {model}</h1>
+        <h1>{brand}-{model}</h1>
         <h1>{year}</h1>
         <h1>{motor}</h1>
     </div>
